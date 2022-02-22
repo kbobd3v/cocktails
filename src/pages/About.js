09a2fd720f2c69816b34277400712c03aee8f82e
kbdev <leonard@airp.ws>
2022-02-22 14:49:25 -0500

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Fusce ac ex ac nibh dictum mattis. Phasellu ultricies auctor odio, a euismod lectus ultricies vel. Sed eget commodo mi. Sed finibus velit nunc, at efficitur velit pulvinar ut. Fusce volutpat ante non dui semper, non tempor felis pretium. Integer in sem bibendum, malesuada leo in, accumsan nisl. Cras eu tellus blandit, blandit enim sit amet, pretium dui. Nullam tellus eros, feugiat vel sollicitudin nec, cursus eu metus. Aliquam vel mi diam. Praesent lobortis purus et magna bibendum ultricies.Fusce ac ex ac nibh dictum mattis. Phasellus ultricies auctor odio, a euismod lectus ultricies vel. Sed eget commodo mi. Sed finibus velit nunc, at efficitur velit pulvinar ut. Fusce volutpat ante non dui semper, non tempor felis pretium. Integer in sem bibendum, malesuada leo in, accumsan nisl. Cras eu tellus blandit, blandit enim sit amet, pretium dui.
+      </p>
+    </section>
+  )
+}
+
+export default About
